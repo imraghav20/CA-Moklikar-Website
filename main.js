1133a0132557039cf0318f5916b91708b5e234eb
imraghav20 <raghav20nov@gmail.com>
2020-09-14 14:08:13 +0530

    added active class in navbar

---
+++ main.js
@@ -0,0 +1,26 @@
+var topMenu = $("#top-menu"),
+    topMenuHeight = topMenu.outerHeight() + 15,
+
+    menuItems = topMenu.find("a"),
+
+    scrollItems = menuItems.map(function () {
+        var item = $($(this).attr("href"));
+        if (item.length) { return item; }
+    });
+
+$(window).scroll(function () {
+
+    var fromTop = $(this).scrollTop() + topMenuHeight;
+
+    var cur = scrollItems.map(function () {
+        if ($(this).offset().top < fromTop)
+            return this;
+    });
+
+    cur = cur[cur.length - 1];
+    var id = cur && cur.length ? cur[0].id : "";
+
+    menuItems
+        .parent().removeClass("active")
+        .end().filter("[href='#" + id + "']").parent().addClass("active");
+})
